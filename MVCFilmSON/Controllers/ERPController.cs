ea44e710b88e55509ba1ea8ed201f7c0067cb594
YunusDinc <yunusdinc1988@hotmail.com>
2016-11-21 13:06:45 +0200

    setup

---
+++ MVCFilmSON/Controllers/ERPController.cs
@@ -0,0 +1,36 @@
+﻿using MVCFilmSON.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFilmSON.Controllers
+{
+    public class ERPController : Controller
+    {
+        // GET: ERP
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Yerli()
+        {
+            ApplicationDbContext ctx = new ApplicationDbContext();
+            return View(ctx.Filmler.Where(x => !x.Yabanci).ToList());
+        }
+
+        public ActionResult Yabanci()
+        {
+            ApplicationDbContext ctx = new ApplicationDbContext();
+            return View(ctx.Filmler.Where(x => x.Yabanci).ToList());
+        }
+
+        public ActionResult Yil(short y)
+        {
+            ApplicationDbContext ctx = new ApplicationDbContext();
+            return View(ctx.Filmler.Where(x => x.Yil == y).ToList());
+        }
+    }
+}
